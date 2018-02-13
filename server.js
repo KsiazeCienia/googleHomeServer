cb997e5b4d674c71e610d8c23abbd7d022588a5e
marcin <mwloczko@gmail.com>
2018-02-13 16:51:34 +0100

    complete server error handler

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var BadRequest = require('./serverError').BadRequest;
+var serverError = require('./serverError');
 var googleHome = require('./googleHome')();
 var EnableNotification = require('./enableNotification');
 var UnableNotification = require('./unableNotification');
@@ -24,19 +24,28 @@ app.get('/notifications/off', function (req, res) {
 
 app.post('/readmessage', function (req, res) {
 
-    var emptyBody = new BadRequest();
-    return emptyBody.handle(req, res);
-
-    // if (!req.body) return res.status(400).send('Body can not be empty');
-    console.log(req.body.message);
-    var message = req.body.message;
-
-    if (message) {
-        googleHome.handleNotification(message);
+    // var emptyBody = new BadRequest();
+    // return emptyBody.handle(req, res);
+    var result = serverError.isProperRequest(req);
+    if (result.isProper) {
+        console.log('tak');
         return res.sendStatus(200);
     } else {
-        return res.status(400).send('Request has to contain \'message\' key');
+        console.log('nie');
+        console.log(result.serverError.getErrorCode());
+        console.log(result.serverError.getErrorMessage());
+        return res.status(result.serverError.getErrorCode()).send(result.serverError.getErrorMessage());
     }
+    // if (!req.body) return res.status(400).send('Body can not be empty');
+    // console.log(req.body.message);
+    // var message = req.body.message;
+    //
+    // if (message) {
+    //     googleHome.handleNotification(message);
+    //     return res.sendStatus(200);
+    // } else {
+    //     return res.status(400).send('Request has to contain \'message\' key');
+    // }
 })
 
 app.listen(serverPort, function() {
--- serverError.js
+++ serverError.js
@@ -1,46 +1,65 @@
 var inherits = require('util').inherits;
 
-// var Result = {
-//     isProper = True;
-//     serverError = null;
-// }
+var result = {
+    isProper: true,
+    serverError: null
+}
 
 function ServerError () {
     this.successor = null;
+    this.errorMessage = '';
+    this.errorCode = 0;
+}
+
+ServerError.prototype.getErrorCode = function () {
+    return this.errorCode;
+}
+
+ServerError.prototype.getErrorMessage = function () {
+    return this.errorMessage;
 }
 
 ServerError.prototype.setSuccessor = function (successor) {
     this.successor = successor;
 }
 
-ServerError.prototype.handle = function (req, res) {
-    return this.successor.handle(req, res);
+ServerError.prototype.handle = function (req) {
+    return this.successor.handle(req);
 }
 
 function EmptyBody() {
     ServerError.call(this);
+    this.errorMessage = 'Body can not be empty';
+    this.errorCode = 400;
 }
 inherits(EmptyBody, ServerError);
 
-EmptyBody.prototype.handle = function (req, res) {
+EmptyBody.prototype.handle = function (req) {
     if (!req.body) {
-        return res.status(400).send('Body can not be empty');
+        // var result = new Result();
+        result.isProper = false;
+        result.serverError = this;
+        return result;
     } else {
-        // return Result.isProper = True;
-        return this.successor.handle(req, res);
+        return this.successor.handle(req);
     }
 }
 
 function BadRequest() {
     ServerError.call(this);
+    this.errorMessage = 'Request has to contain \'message\' key';
+    this.errorCode = 400;
 }
 inherits(BadRequest, ServerError);
 
-BadRequest.prototype.handle = function (req, res) {
+BadRequest.prototype.handle = function (req) {
     if (!req.body.message) {
-        return res.status(400).send('Request has to contain \'message\' key');
+        // var result = new Result();
+        result.isProper = false;
+        result.serverError = this;
+        return result;
     } else {
-        return null;
+        return result;
     }
 }
 
@@ -53,10 +72,10 @@ function ServerErrorStack() {
     this.head = emptyBody;
 }
 
-var isProperRequest = function(req, res) {
+var isProperRequest = function(req) {
     var serverErrorStack = new ServerErrorStack();
-
+    return serverErrorStack.head.handle(req);
 }
 
-module.exports.EmptyBody = EmptyBody;
-module.exports.BadRequest = BadRequest;
+module.exports.isProperRequest = isProperRequest;
+module.exports.result = result;
